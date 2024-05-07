f853f2aea4b9f1bb06ec78b40b6c62ebe2d5e385
Owais Ahmad <owaisahmad014@gmail.com>
2024-05-07 11:28:50 +0500

    completed

--- queueUsingLinkedList.cpp
+++ queueUsingLinkedList.cpp
@@ -6,7 +6,7 @@ struct Node
     int data;
     Node *next;
 };
-Node *front = NULL, *rear = NULL;
+Node *front = NULL, *rear = NULL; // global
 
 void enqueue(int x)
 {
@@ -31,10 +31,47 @@ void enqueue(int x)
 
 int dequeue()
 {
-    
+    int x = -1;
+    Node *t;
+
+    if(front == NULL)
+        cout<<"Queue is Empty"<<endl;
+    else
+    {
+        x = front->data;
+        t = front;
+        front = front->next;
+        delete(t);
+    }
+    return x;
 }
+
+void Display()
+{
+    Node *p = front;
+    while(p != NULL)
+    {
+        cout<<p->data<<" ";
+        p = p->next;
+    }
+}
+
 
 int main()
 {
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+    enqueue(40);
+    enqueue(50);
+
+
+    cout<<"Queue values before dequue : ", Display();
+
+    dequeue();
+    cout<<endl;
+
+    cout<<"Queue values after dequue : ",Display();
+
     return 0;
 }
